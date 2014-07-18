f0c52a858d9da84f217616598028d2d97358b2fb
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-07-18 17:00:23 +0200

    llvm: Improve compatibility with Mac OS X

--- packages/llvm/llvm.3.4/files/compile.sh
+++ packages/llvm/llvm.3.4/files/compile.sh
@@ -2,11 +2,26 @@
 
 version=3.4
 
-for config in llvm-config-$version llvm-config-mp-$version llvm-config; do
+brew_llvm_config=/usr/local/Cellar/llvm/3.4.*/bin/llvm-config
+if [ ! -x $brew_llvm_config ]
+then
+    brew_llvm_config=
+fi
+
+common_configure="./configure CC=gcc CXX=g++ --prefix=$2 --disable-doxygen --disable-docs --enable-static --with-ocaml-libdir=$3/llvm"
+
+for config in llvm-config-$version llvm-config-mp-$version $brew_llvm_config llvm-config; do
     case `$config --version` in
         $version|$version.*)
-            $1 -C bindings/ocaml build SYSTEM_LLVM_CONFIG=$config
-            $1 -C bindings/ocaml install SYSTEM_LLVM_CONFIG=$config
+            case `uname -s` in
+                Darwin)
+                    $common_configure --disable-shared;;
+                *)
+                    $common_configure --enable-shared;;
+            esac
+            $1 -C bindings/ocaml build SYSTEM_LLVM_CONFIG="$config"
+            $1 -C bindings/ocaml install SYSTEM_LLVM_CONFIG="$config"
+            cp "$3/llvm/META.llvm" "$3/llvm/META"
             exit 0;;
         *)
             continue;;
